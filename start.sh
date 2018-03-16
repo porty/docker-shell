511ffa42bc9334b287d00e7fd643835266bc5ac8
Robert McNeil <pppppppman@gmail.com>
2018-03-16 15:10:43 +1100

    Initial commit

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [ ! -f /etc/ssh/ssh_host_rsa_key ] ; then
+    ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+    ssh-keygen -f /etc/ssh/ssh_host_dsa_key -N '' -t dsa
+fi
+
+/usr/sbin/sshd -D
